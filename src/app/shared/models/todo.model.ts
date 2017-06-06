35e9a8b514a8846370387b0baac3609c454f0a71
elias-garcia <elias.garcia@udc.es>
2017-06-07 00:46:00 +0200

    Responsive tables

--- src/app/shared/models/todo.model.ts
+++ src/app/shared/models/todo.model.ts
@@ -4,12 +4,14 @@ export class Todo {
   description: string;
   completed: boolean;
   editing: boolean;
+  deleting: boolean;
 
   constructor(description: string) {
     this.id = Date.now();
     this.description = description;
     this.completed = false;
     this.editing = false;
+    this.deleting = false;
   }
 
 };
